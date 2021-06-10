757611e3f301b5f17565509053546db6933a64e4
amit-a-aggarwal <amit.a.aggarwal@pwc.com>
2021-06-10 18:08:03 +0530

    Added Watson Assistant

--- bots/dispatch_bot.py
+++ bots/dispatch_bot.py
@@ -4,11 +4,40 @@
 from azure.cognitiveservices.language.luis.runtime.models import LuisResult
 
 from botbuilder.ai.luis import LuisApplication, LuisRecognizer, LuisPredictionOptions
-from botbuilder.ai.qna import QnAMaker, QnAMakerEndpoint
+# from botbuilder.ai.qna import QnAMaker, QnAMakerEndpoint
 from botbuilder.core import ActivityHandler, TurnContext, RecognizerResult
 from botbuilder.schema import ChannelAccount
+from ibm_watson import AssistantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 from config import DefaultConfig
+
+api_key = 'zPG3YBJXr_O8kuGpw3aSZpkLJAHM1RSLS-Zl9_dkhW0V'
+service_url = 'https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/78440ed4-31fc-4d6e-8084-9e96b145d110'
+workspace_id = "ece3b15f-9c3b-40cd-8d7c-3524cf21c8a2"
+
+def watson_assistant(api_key, service_url):
+    authenticator = IAMAuthenticator(api_key)
+    assistant = AssistantV1(
+        version='2020-04-01',
+        authenticator=authenticator
+    )
+
+    assistant.set_service_url(service_url)
+    assistant.set_disable_ssl_verification(True)
+
+    return assistant
+
+assistant = watson_assistant(api_key, service_url)
+# authenticator = IAMAuthenticator(api_key)
+# assistant = AssistantV1(
+#     version='2020-04-01',
+#     authenticator = authenticator
+# )
+#
+# assistant.set_service_url(service_url)
+# assistant.set_disable_ssl_verification(True)
+
 
 
 class DispatchBot(ActivityHandler):
@@ -46,6 +75,7 @@ class DispatchBot(ActivityHandler):
     async def on_message_activity(self, turn_context: TurnContext):
         # First, we use the dispatch model to determine which cognitive service (LUIS or QnA) to use.
         recognizer_result = await self.recognizer.recognize(turn_context)
+        # print("recognizer_result: ", recognizer_result.__dict__)
 
         # Top intent tell us which cognitive service to use.
         intent = LuisRecognizer.top_intent(recognizer_result)
@@ -94,24 +124,44 @@ class DispatchBot(ActivityHandler):
     async def _process_weather(
         self, turn_context: TurnContext, luis_result: LuisResult
     ):
+
+        # Earlier
+        # await turn_context.send_activity(
+        #     f"ProcessWeather top intent {luis_result.top_scoring_intent}."
+        # )
+        #
+        # # extra
+        # # intents_list = "\n\n".join(
+        # #     [intent_obj.intent for intent_obj in luis_result.intents]
+        # # )
+        # # await turn_context.send_activity(
+        # #     f"ProcessWeather intents detected: {intents_list}."
+        # # )
+        #
+        # if luis_result.entities:
+        #     entities_list = "\n\n".join(
+        #         [entity_obj.entity for entity_obj in luis_result.entities]
+        #     )
+        #     await turn_context.send_activity(
+        #         f"ProcessWeather entities were found in the message: {entities_list}."
+        #     )
+
+        #Calling Watson
+        text = turn_context.activity.text
+        response = assistant.message(workspace_id=workspace_id, input={'text': text}).get_result()
+
         await turn_context.send_activity(
-            f"ProcessWeather top intent {luis_result.top_scoring_intent}."
+            f"ProcessWeather top intent {response['intents'][0]['intent']}."
         )
 
-        intents_list = "\n\n".join(
-            [intent_obj.intent for intent_obj in luis_result.intents]
-        )
-        await turn_context.send_activity(
-            f"ProcessWeather intents detected: {intents_list}."
-        )
-
-        if luis_result.entities:
+        if response['entities']:
             entities_list = "\n\n".join(
-                [entity_obj.entity for entity_obj in luis_result.entities]
-            )
+                        [entity_obj['value'] for entity_obj in response['entities']]
+                    )
+
             await turn_context.send_activity(
-                f"ProcessWeather entities were found in the message: {entities_list}."
-            )
+                    f"ProcessWeather entities were found in the message: {entities_list}."
+                )
 
     async def _process_sample_qna(self, turn_context: TurnContext):
         results = await self.qna_maker.get_answers(turn_context)
